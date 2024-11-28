06ebf97eab3a74bacc83ea2fc6f5c92632723274
Stefan <stefancool00@gmail.com>
2024-11-27 20:28:37 -0800

    Exercise 1 done

---
+++ Mini 1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace exercise
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string name = "Stefan";
+            string phoneNumber = "306-511-5225";
+            int age = 24;
+            Console.WriteLine(name);
+            Console.WriteLine(phoneNumber);
+            Console.WriteLine(age);
+        }
+    }
+}
