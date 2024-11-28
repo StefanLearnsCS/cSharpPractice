d4426b22b2cb1d351a8d3739c56494b0f127a340
Stefan <stefancool00@gmail.com>
2024-11-27 21:41:21 -0800

    Exercise 4 done

---
+++ Mini 4/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise
+{
+  class Program 
+    {
+        static void Main(string[] args)
+        {
+            bool threeDiv;
+            bool fiveDiv;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                threeDiv = i % 3 == 0;
+                fiveDiv = i % 5 == 0;
+                if (threeDiv && fiveDiv)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (threeDiv)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (fiveDiv)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
