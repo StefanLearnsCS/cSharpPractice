aba6dcb9420fe23650a4103c1de5dd51f06ccb92
Stefan <stefancool00@gmail.com>
2024-11-29 12:51:55 -0800

    Exercise 8 done

---
+++ Mini 8/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise
+{
+  class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Triangle Height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Triangle Width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            int area = calculateArea(height, width);
+
+            Console.Write($"Triangle Area is: {area}");
+        }
+
+        static int calculateArea(int height, int width)
+        {
+            return height*width/2;
+        }
+    }
+}
