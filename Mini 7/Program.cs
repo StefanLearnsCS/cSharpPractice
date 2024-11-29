4fb83f4d717150b01989eb68197da75e9f27fd4d
Stefan <stefancool00@gmail.com>
2024-11-28 21:10:26 -0800

    Exercise 7 done

---
+++ Mini 7/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise
+{
+  class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Give a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Give a length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int[] numarray = new int[length];
+
+            for (int i = 1; i <= numarray.Length; i++)
+            {
+                numarray[i-1] = num * i;
+            }
+
+            foreach (var item in numarray)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
